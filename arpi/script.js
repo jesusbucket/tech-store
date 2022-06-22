ab7f73264cdaeb9e35d222d194725798d94d6875
jesusbucket <91190645+jesusbucket@users.noreply.github.com>
2022-06-22 21:22:05 +0200

    Add files via upload

---
+++ arpi/script.js
@@ -0,0 +1,28 @@
+document.getElementById("button").addEventListener('click',load)
+function load(){;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','tech.json',true);
+    xhr.onload= function(){
+        if(this.status==200){
+            let items = JSON.parse(this.responseText);
+            let output="";
+            for(let i in items){
+                output +=
+                '<div class=item>'+
+                '<img src='+items[i].image+'width=256 height=256>'+
+                '<ul>'+
+                '<li>Product:'+
+                items[i].name+
+                '</li>'+
+                '<li>Description:'+
+                items[i].description+
+                '</li>'+
+                '</ul>'+
+                '</div>'
+            }
+            document.getElementById("items-container").innerHTML=output
+        }
+    }
+    xhr.send()
+}
+
